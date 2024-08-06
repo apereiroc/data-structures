54703d6fe968d9993963f90278e57e972bef14f2
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-08-06 13:35:55 +0200

    feat: add move assignment, move constructor, pop_back and refactor some variable names

--- dynamic_array.h
+++ dynamic_array.h
@@ -13,7 +13,35 @@ public:
       : capacity_(new_capacity), size_(0),
         data_(std::make_unique<T[]>(new_capacity)) {}
 
-  void resize(const std::size_t &new_capacity) {
+  // Move assignment operator
+  Array &operator=(Array &&other) noexcept {
+    if (this != &other) {
+      capacity_ = other.capacity_;
+      size_ = other.size_;
+      data_ = std::move(other.data_);
+
+      // Reset other
+      other.capacity_ = 0;
+      other.size_ = 0;
+    }
+    return *this;
+  }
+
+  // Move constructor
+  Array(Array &&other) noexcept
+      : capacity_(other.capacity_), size_(other.size_),
+        data_(std::move(other.data_)) {
+    other.capacity_ = 0;
+    other.size_ = 0;
+  }
+
+  // Deleted copy assignment operator to prevent copying
+  Array &operator=(const Array &other) = delete;
+
+  // Deleted copy constructor to prevent copying
+  Array(const Array &other) = delete;
+
+  inline void resize(const std::size_t &new_capacity) {
     auto new_data{std::make_unique<T[]>(new_capacity)};
 
     for (std::size_t i = 0; i < size_; i++) {
@@ -24,7 +52,7 @@ public:
     capacity_ = new_capacity;
   }
 
-  void push(const T &val) {
+  inline void push_back(const T &val) {
     if (size_ == capacity_)
       resize(2 * capacity_);
 
@@ -32,27 +60,35 @@ public:
     size_++;
   }
 
-  template <typename... Values> void push(const Values &...vals) {
-    (push(vals), ...);
+  template <typename... Values> inline void push_back(const Values &...vals) {
+    (push_back(vals), ...);
   }
 
-  T &operator[](std::size_t idx) {
+  inline void pop_back() {
+    if (size_ > 0) {
+      --size_;
+    }
+  }
+
+  inline T &operator[](std::size_t idx) {
+    if (idx >= size_) {
+      std::cout << "ERROR: trying to access idx " << idx << " but size is "
+                << size_ << '\n';
+      throw std::out_of_range("Index out of range");
+    }
+    return data_[idx];
+  }
+
+  inline const T &operator[](const std::size_t &idx) const {
     if (idx >= size_) {
       throw std::out_of_range("Index out of range");
     }
     return data_[idx];
   }
 
-  const T &operator[](const std::size_t &idx) const {
-    if (idx >= size_) {
-      throw std::out_of_range("Index out of range");
-    }
-    return data_[idx];
-  }
+  inline void clear() { size_ = 0; }
 
-  void clear() { size_ = 0; }
-
-  void print() {
+  inline void print() {
     std::string print_str{"[ "};
 
     for (std::size_t i = 0; i < size_; i++) {
@@ -64,7 +100,7 @@ public:
     std::cout << print_str << '\n';
   }
 
-  std::size_t size() { return size_; }
+  inline std::size_t size() { return size_; }
 
-  std::size_t capacity() { return capacity_; }
+  inline std::size_t capacity() { return capacity_; }
 };
