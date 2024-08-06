305795e1f673076361809b308ab9e9e488bf1140
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-08-06 13:36:11 +0200

    feat: add more prints

--- dynamic_array.cpp
+++ dynamic_array.cpp
@@ -7,29 +7,30 @@ int main() {
   Array<int> a{5};
   a.print();
 
-  a.push(1, 2, 3);
+  std::cout << "Pushing back 1,2,3\n";
+  a.push_back(1, 2, 3);
   a.print();
 
-  for (int i = 1; i <= 20; i++) {
-    a.push(i);
+  std::cout << "Pushing back 4,5,6... 20\n";
+  for (int i = 4; i <= 20; i++) {
+    a.push_back(i);
   }
-
   a.print();
 
   std::cout << "\nB:\n";
 
   Array<int> b(1);
 
-  b.push(1, 2, 3, 4, 5);
-
+  std::cout << "Pushing 1,2,3,4,5\n";
+  b.push_back(1, 2, 3, 4, 5);
   b.print();
 
+  std::cout << "Clearing\n";
   b.clear();
-
   b.print();
 
-  b.push(1, 2, 3, 4, 5);
-
+  std::cout << "Pushing 1,2,3,4,5\n";
+  b.push_back(1, 2, 3, 4, 5);
   b.print();
 
   return 0;
