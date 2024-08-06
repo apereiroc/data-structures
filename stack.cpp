aec0adb26b7b830cc8e735d886cfee86e35f19eb
Asier Pereiro <asier.pereiro.castro@cern.ch>
2024-08-06 13:36:24 +0200

    feat: add stack class

---
+++ stack.cpp
@@ -0,0 +1,59 @@
+#include "dynamic_array.h"
+
+template <typename T> class Stack {
+private:
+  Array<T> data{};
+  std::size_t top{0};
+
+public:
+  Stack(const std::size_t &size) : data(Array<T>(size)) {}
+
+  Stack() : data(Array<T>()) {}
+
+  inline void push(const T &val) {
+    data.push_back(val);
+    top++;
+  }
+
+  template <typename... Values> inline void push(const Values &...vals) {
+    (this->push(vals), ...);
+  }
+
+  inline T pop() {
+    if (top == 0) {
+      throw std::out_of_range("Stack is empty, cannot pop");
+    }
+    top--;
+    T value = std::move(data[top]);
+    data.pop_back();
+    return value;
+  }
+
+  inline void print() { data.print(); }
+};
+
+int main() {
+  Stack<int> s{};
+
+  s.push(1, 2, 3);
+  std::cout << "Pushing 1, 2, 3:\n";
+  s.print();
+
+  std::cout << "Pushing 4:\n";
+  s.push(4);
+  s.print();
+
+  std::cout << "Pushing 5, 6:\n";
+  s.push(5, 6);
+  s.print();
+
+  std::cout << "Popping:\n";
+  s.pop();
+  s.print();
+
+  std::cout << "Popping:\n";
+  s.pop();
+  s.print();
+
+  return 0;
+}
